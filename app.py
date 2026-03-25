c77216500b1c0078f96ca9e7c2576392f854f613
HARSH PALKRUTWAR <16.prahar@gmail.com>
2026-03-25 21:59:20 +0530

    Change app.run to use host and port

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ def predict():
         return jsonify({'success': False, 'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
